6325eb1f57a006fd10af559d1d71ff9562568b77
luisillo77D <77709381+luisillo77D@users.noreply.github.com>
2024-10-28 09:38:32 -0700

    Update samples.routes.js

--- src/routes/samples.routes.js
+++ src/routes/samples.routes.js
@@ -14,7 +14,7 @@ import { validateSchema } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
-router.get("/samples", authRequired, getSamples);
+router.get("/samples", getSamples);
 router.get("/allsamples", authRequired, getallSamples);
 router.get("/samples/:id", authRequired, getSample);
 router.get("/samplescheck/:status", authRequired, getSamplesChecked);
